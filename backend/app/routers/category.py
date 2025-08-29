7415f936644a4e21a83897a80d4fd642cd7c8f38
JOÃO PEDRO <pdrojoao.slv@gmail.com>
2025-08-29 16:27:57 -0300

    category schema import adjusted

--- backend/app/routers/category.py
+++ backend/app/routers/category.py
@@ -1,11 +1,11 @@
-from app.schemas.category import GetCategories
+from app.schemas.category import GetAllCategoriesResponse
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 
 router = APIRouter(prefix="/category", tags=["category"])
 
 
-@router.get("/", response_model=GetCategories)
+@router.get("/", response_model=GetAllCategoriesResponse)
 def get_all_categories():
 
     return JSONResponse(status_code=200, content={"categories": []})
