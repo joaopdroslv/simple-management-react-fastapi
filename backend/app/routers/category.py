d27ce0f853fe1e4df09914753cc4cdc94d7664a3
JOÃO PEDRO <pdrojoao.slv@gmail.com>
2025-09-05 13:49:38 -0300

    get_categories route created

--- backend/app/routers/category.py
+++ backend/app/routers/category.py
@@ -1,11 +1,26 @@
+from app.database.deps import get_db
+from app.modules import category
 from app.schemas.category import GetCategoriesResponse
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from fastapi.responses import JSONResponse
+from sqlalchemy.orm import Session
 
 router = APIRouter(prefix="/category", tags=["category"])
 
 
-@router.get("/", response_model=GetCategoriesResponse)
-def get_categories():
+@router.get("")
+def get_categories(page: int = 1, limit: int = 10, db: Session = Depends(get_db)):
 
-    return JSONResponse(status_code=200, content={"categories": []})
+    total, db_categories = category.get_categories(page, limit, db)
+
+    return JSONResponse(
+        status_code=200,
+        content={
+            "pagination": {
+                "page": page,
+                "limit": limit,
+                "total": total,
+            },
+            "categories": [c.to_dict() for c in db_categories],
+        },
+    )
